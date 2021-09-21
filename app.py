ad9bbe6d3d0fc661d52ebc12cc157e2e425307a9
Marc Gravel <gravel.m@outlook.com>
2021-09-21 16:36:08 -0700

    handle divide by 0 error

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import division
 import multiplication
 import subtraction
 
+
 keepRunning = True
 
 while keepRunning:
@@ -28,6 +29,13 @@ while keepRunning:
                 elif (user_choice == "3"):
                     print(num1, "*", num2, "=", multiplication.multiply(num1, num2))
                 elif (user_choice == "4"):
+                    if(num2 == 0):
+                        print("You cannot divide by 0, try again")
+                        num2 = float(input("Enter second number: "))
+                        if (num2 == 0):
+                            print("You don't listen..Goodbye")
+                            keepRunning = False
+                            break
                     print(num1, "/", num2, "=", division.divide(num1, num2))
 
                 start_again = input("Would you like to calculate again? (Y/N):  ")
